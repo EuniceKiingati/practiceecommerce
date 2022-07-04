fe273625f6e3135c1a6b337746d2fce578565ef7
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2022-07-04 06:47:37 +0300

    upto defining models

---
+++ tags/models.py
@@ -0,0 +1,18 @@
+from email.headerregistry import ContentTypeHeader
+from tkinter import CASCADE
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+class Tag(models.Model):
+    label=models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    #what item applied to what object
+    tag=models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type=models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id=models.PositiveIntegerField()
+    content_object=GenericForeignKey()
